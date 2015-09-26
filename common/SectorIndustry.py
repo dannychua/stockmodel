89b4a626c6f0fe91c3895bbc80c9e5fd3dbcd6ab
Xiaodan Zhang <xiaodanright@gmail.com>
2015-09-26 16:54:41 -0400

    no message

--- common/SectorIndustry.py
+++ common/SectorIndustry.py
@@ -1,1 +1,34 @@
-__author__ = 'xiaodan'
+# %% header
+# % handle sector or industry or concept
+# % date: 8/15/2015
+import time
+import GlobalConstant
+from QTimeSeries import QTimeSeries
+# %%
+#
+# % QDate has the format of yyyymmdd
+
+class SectorIndustry:
+    def __init__(self):
+        #% WIND sector/industry data
+        return SectorIndustry.loadWINDIndustry()
+
+    @staticmethod
+    def loadWINDIndustry():
+        WINDIndustryCache = 'd:\ChinaA\data\WINDIndustry'
+        #%save(WINDIndustryCache, 'WINDIndustry');
+        #load(WINDIndustryCache)
+        sqlString = 'select S_INFO_WINDCODE, WIND_IND_CODE, ENTRY_DT, REMOVE_DT, CUR_SIGN from WindDB.dbo.ASHAREINDUSTRIESCLASS'
+        curs = GlobalConstant.DBCONN_WIND.cursor()
+        curs.execute(sqlString)
+        windIndustry = {}
+        for row in curs.fetchall():
+            windID, code, entryDt, removeDt, cur_sign = row
+            entryDt = time.strptime(entryDt, '%Y%m%d')
+            if windID in windIndustry:
+                ts = windIndustry[windID]
+                ts.add(entryDt, code)
+            else:
+                ts = QTimeSeries(entryDt, code);
+                windIndustry[windID] = ts
+        return windIndustry
