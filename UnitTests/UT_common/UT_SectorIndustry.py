43bc5669740d75a1882195fb721c15f573be23a0
xxia <xiaofeng.xia@gmail.com>
2015-09-29 22:21:06 -0400

    unit test for SectorIndustry

--- UnitTests/UT_common/UT_SectorIndustry.py
+++ UnitTests/UT_common/UT_SectorIndustry.py
@@ -1,12 +1,13 @@
 from common.SectorIndustry import *
 
 def UnitTests():
+    sectorInd = SectorIndustry();
     # 1st time access,if no data file exists, run database query
-    numDts = len(SectorIndustry.WINDIndustry);
-    print("1st time Num of Dates: " + numDts)
+    numDts = len(sectorInd.WINDIndustry);
+    print("1st time Num of Dates: " + str(numDts))
     # 2nd time access, load it from the file
-    numDts = len(SectorIndustry.WINDIndustry);
-    print("2nd time Num of Dates: " + numDts)
+    numDts = len(sectorInd.WINDIndustry);
+    print("2nd time Num of Dates: " + str(numDts))
 
 if __name__ == '__main__':
     UnitTests()
