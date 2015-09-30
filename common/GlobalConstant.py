164642233a52e2f4cc5a4ad1c92493aa332e92d3
Danny <decflow@gmail.com>
2015-09-30 02:40:42 -0400

    Remove Date code to avoid circular dependency situation
    Also removed remaining stale code (ad-hoc tests) ported from matlab

--- common/GlobalConstant.py
+++ common/GlobalConstant.py
@@ -1,24 +1,6 @@
 __author__ = 'xiaodan'
-from datetime import datetime
-import QDate
 from local_env import DBCONN_WIND
 from local_env import BASE_DIR, CODE_DIR, DATA_DIR
 # %% header
 # % store all constant variables that can be accessed globally
 # % date: 9/06/2015
-
-#locations
-
-
-
-#test start and end date
-TestStartDate = datetime(2008, 12, 31)
-TestEndDate = datetime(2014, 12, 31)
-#exchange trading dates
-TradingDates = QDate.GetAllTradingDays()
-
-#holding structure
-Holding = ['StockID', 'Weight', 'Shares', 'MarketValue']
-
-
-
