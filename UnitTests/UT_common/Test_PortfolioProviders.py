a82ace7e56456b2ce1d9add0b836628fe5792171
Danny <decflow@gmail.com>
2015-10-03 01:07:23 -0400

    Improved unit test for PortfolioProviders

--- UnitTests/UT_common/Test_PortfolioProviders.py
+++ UnitTests/UT_common/Test_PortfolioProviders.py
@@ -1,13 +1,14 @@
+from nose.tools import assert_equal
 from common.PortfolioProviders import PortfolioProviders
 
 
 def test_getClosingPx():
 	closingPx_ts = PortfolioProviders.getClosingPx('000016.SH')
-	assert closingPx_ts.qseries.iloc[0] == 1436.885
+	assert_equal(closingPx_ts.qseries.iloc[0], 1436.885)
 
 
 def test_getA50():
 	portfolioProvider = PortfolioProviders.getA50()
 	portfolio = portfolioProvider.GetPortfolioOn('20090401')
 	holdings = portfolio.getHoldings()
-	assert holdings[0] == {'Weight': 0.274, 'StockID': '601991.SH'}
+	assert_equal(holdings[0], {'Weight': 0.274, 'StockID': '601991.SH'})
