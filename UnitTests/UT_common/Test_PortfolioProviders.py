7ddf711bc3ae2afaf74ce9734aeb78a3d30a429e
Danny <decflow@gmail.com>
2015-10-08 18:55:48 -0400

    PortfolioProviders UT fail due to change in Portfolio.py

--- UnitTests/UT_common/Test_PortfolioProviders.py
+++ UnitTests/UT_common/Test_PortfolioProviders.py
@@ -10,5 +10,5 @@ def test_getClosingPx():
 def test_getA50():
 	portfolioProvider = PortfolioProviders.getA50()
 	portfolio = portfolioProvider.GetPortfolioOn('20090401')
-	holdings = portfolio.getHoldings()
+	holdings = portfolio.Holdings()
 	assert_equal(holdings[0], {'Weight': 0.274, 'StockID': '601991.SH'})
