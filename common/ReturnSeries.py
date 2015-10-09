4b89b480c7b1a8746e26f92115558208def57a80
Danny <decflow@gmail.com>
2015-10-09 02:21:31 -0400

    ReturnSeries broken due to change in QTimeSeries.py

--- common/ReturnSeries.py
+++ common/ReturnSeries.py
@@ -33,9 +33,9 @@ class ReturnSeries(QTimeSeries):
 
     def __calc(self):
         """ Calculates various statistics of the portfolio's returns """
-        if self.qseries.shape[0] >= 2:
+        if self.Series.shape[0] >= 2:
 
-            daysDelta = self.qseries.iloc[1] - self.qseries.iloc[0]
+            daysDelta = self.Series.iloc[1] - self.Series.iloc[0]
             if daysDelta == 1 | daysDelta == 3:     # If weekday
                 self.__annScalar = 252
             if daysDelta == 7:                      # If weekly
@@ -47,17 +47,17 @@ class ReturnSeries(QTimeSeries):
 
 
             # Calculate statistics
-            mean = self.qseries.mean()
-            std = self.qseries.std()
-            self.__returns = self.qseries.tolist()
+            mean = self.Series.mean()
+            std = self.Series.std()
+            self.__returns = self.Series.tolist()
             self.__annMean = mean * self.__annScalar
             self.__annStd = std * self.__annScalar
             self.__sr = math.sqrt(self.__annScalar) * mean / std
 
             # TODO: need to handle the number of periods in a year
             numPeriods = len(self.__returns)
-            self.__compCumReturns = self.qseries.cumprod()
-            self.__cumReturns = self.qseries.cumsum()
+            self.__compCumReturns = self.Series.cumprod()
+            self.__cumReturns = self.Series.cumsum()
 
 
     def plot(self, type='cr', desc=''):
@@ -81,7 +81,7 @@ class ReturnSeries(QTimeSeries):
 
 
     @property
-    def annMean(self):
+    def annMean():
         """ Annualized mean """
         if not hasattr(self, '__annMean'):
             self.__calc()
@@ -132,6 +132,6 @@ class ReturnSeries(QTimeSeries):
 
 
 
-# returnSeries = ReturnSeries(['2010-01-01', '2010-01-02', '2010-01-03', '2010-01-04'], [1,2,3,4])
+# returnSeries = ReturnSeries(['20100101', '20100102', '20100103', '20100104'], [1,2,3,4])
 # print returnSeries.returns 
 # print returnSeries.compCumReturns
