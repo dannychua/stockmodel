d21d00ef99934feef0575df65c8ac48218e607b1
xxia <xiaofeng.xia@gmail.com>
2015-10-20 22:37:26 -0400

    added __getIndicatorFromDB_All in WindIndicators.py, which converts the data to a time series of data frame, where each data frame has row "StockID" and indicators columns "BP", ... The function is NOT tested, and it is very unlikely to work at this point. My intention is to lay out the data structure. As this is a new function, all existing codes are not changed.

--- source/common/Factorlib/WINDIndicators.py
+++ source/common/Factorlib/WINDIndicators.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import GlobalConstant as GlobalConstant
+from Utils import Str2Date
 
 
 def BPCalc(stockID, date):
@@ -81,4 +82,26 @@ def __getIndicatorFromDB(stockID, date, fieldName):
     if rownum < 1:
         return np.nan
     val = df.iloc[0]['F']
+    return val
+
+
+indicatorsData = None
+def __getIndicatorFromDB_All(stockID, date, fieldName):
+    """ Retrieve indicators from WINDDB """
+    global indicatorsData
+    if indicatorsData is None:
+        sqlQuery = """
+           select TRADE_DT, s_info_windcode StockID, 1/s_val_pb_new BP, 1/s_val_pe EP, 1/s_val_pe_ttm EPttm, 1/s_val_pcf_ncf CFP,
+            1/s_val_pcf_ncfttm CFPttm, 1/s_val_pcf_ocf OCFP, 1/s_val_pcf_ocfttm OCFPttm, 1/s_val_ps SalesP,
+            1/s_val_ps_ttm SalesPttm, s_dq_turn Turnover, s_dq_freeturnover FreeTurnover, 1/s_price_div_dps DividendYield
+           from WINDDB.DBO.AShareEODDerivativeIndicator
+           where TRADE_DT>'%s' """ % GlobalConstant.TestStartDate
+        table = pd.read_sql(sqlQuery, GlobalConstant.DBCONN_WIND, parse_dates = {'TRADE_DT':'%Y%m%d'})
+        dates = table.TRADE_DT.tolist().unique()
+        dfArray = []
+        for dt in dates:
+            dfArray.append(pd.DataFrame(table['TRADE_DT' == dt], index =table.StockID))
+        indicatorsData = pd.Series(dfArray, index = dates)
+    dt = Str2Date(date)
+    val = indicatorsData[date][stockID][fieldName]
     return val
