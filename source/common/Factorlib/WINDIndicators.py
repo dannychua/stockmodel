5e00b51188d49ea3d346808d2ed311f55c1a172f
Danny <decflow@gmail.com>
2015-10-24 16:00:13 -0400

    Optimize DB dump into HDFStore

--- source/common/Factorlib/WINDIndicators.py
+++ source/common/Factorlib/WINDIndicators.py
@@ -86,79 +86,97 @@ def __getIndicatorFromDB(stockID, date, fieldName):
     return val
 
 
+# Loading of this module will cause the creation of this cache
 indicatorsData = None
-def __getIndicatorFromDB_All(stockID, date, fieldName):
+
+def __getIndicatorFromDB_All(stockID, date, fieldName, purgeCache=False):
     """ Retrieve indicators from WINDDB """
     
     global indicatorsData
 
-    from datetime import datetime
-    GlobalConstant.TestStartDate = datetime(2014, 12, 31)
+    # If cache not in memory
+    if indicatorsData is None or purgeCache == True:
 
-    if indicatorsData is None:
-        # Query Database
-        sqlQuery = """
-           select TRADE_DT, s_info_windcode StockID, 1/s_val_pb_new BP, 1/s_val_pe EP, 1/s_val_pe_ttm EPttm, 1/s_val_pcf_ncf CFP,
-            1/s_val_pcf_ncfttm CFPttm, 1/s_val_pcf_ocf OCFP, 1/s_val_pcf_ocfttm OCFPttm, 1/s_val_ps SalesP,
-            1/s_val_ps_ttm SalesPttm, s_dq_turn Turnover, s_dq_freeturnover FreeTurnover, 1/s_price_div_dps DividendYield
-           from WINDDB.DBO.AShareEODDerivativeIndicator
-           where TRADE_DT>'%s' """ % GlobalConstant.TestStartDate
-        df = pd.read_sql(sqlQuery, GlobalConstant.DBCONN_WIND, parse_dates = {'TRADE_DT':'%Y%m%d'})
+        # HDF5 Database Cache
+        hdfPath = os.path.join(GlobalConstant.DATA_DIR, 'db.h5')
+        print hdfPath
+        store = pd.HDFStore(hdfPath)
 
-        # Create list of df
-        dates = df.TRADE_DT.unique()
-        dfArray = [df[df['TRADE_DT'] == dt].set_index('StockID') for dt in dates]
+        # If cache not in DFStore
+        if 'df_indicators' not in store or purgeCache == True:
 
-        # Create Panel
-        indicatorsData = pd.Series(dfArray, index=dates)
+            # Start with a clean empty DataFrame
+            store.put('df_indicators', pd.DataFrame(), 'table')   
 
-    date = Str2Date(date)
-    return indicatorsData[date].ix[stockID][fieldName]
+            # Query Database
+            sqlQuery = """
+               select TRADE_DT, s_info_windcode StockID, 1/s_val_pb_new BP, 1/s_val_pe EP, 1/s_val_pe_ttm EPttm, 1/s_val_pcf_ncf CFP,
+                1/s_val_pcf_ncfttm CFPttm, 1/s_val_pcf_ocf OCFP, 1/s_val_pcf_ocfttm OCFPttm, 1/s_val_ps SalesP,
+                1/s_val_ps_ttm SalesPttm, s_dq_turn Turnover, s_dq_freeturnover FreeTurnover, 1/s_price_div_dps DividendYield
+               from WINDDB.DBO.AShareEODDerivativeIndicator
+               where TRADE_DT>'%s' """ % GlobalConstant.TestStartDate
 
+            # SELECT from DB in chunks and persist chunk to disk before retriving next chunk from DB
+            for chunk in pd.read_sql(sqlQuery, GlobalConstant.DBCONN_WIND, parse_dates = {'TRADE_DT':'%Y%m%d'}, chunksize=10000):
+                
+                # Convert None to NaN since Pandas cant tell dtypes if entire column retrieved from database are NULLs
+                df = chunk.fillna(value=np.nan).set_index('TRADE_DT')
 
+                # Persist to HDF5 Store
+                store.append('df_indicators', df)
 
-def __getIndicatorFromDB_byStockID(stockID, fieldName):
-    """ Retrieve indicators from WINDDB """
+            store.close()
+
+        else :
+            # Retrieve from HDFS
+            indicatorsData = store.get('df_indicators')
     
-    # Query Database
-    sqlQuery = """
-       select TRADE_DT, s_info_windcode StockID, 1/s_val_pb_new BP, 1/s_val_pe EP, 1/s_val_pe_ttm EPttm, 1/s_val_pcf_ncf CFP,
-        1/s_val_pcf_ncfttm CFPttm, 1/s_val_pcf_ocf OCFP, 1/s_val_pcf_ocfttm OCFPttm, 1/s_val_ps SalesP,
-        1/s_val_ps_ttm SalesPttm, s_dq_turn Turnover, s_dq_freeturnover FreeTurnover, 1/s_price_div_dps DividendYield
-       from WINDDB.DBO.AShareEODDerivativeIndicator
-       where s_info_windcode='%s'
-       and TRADE_DT>'%s' """ % (stockID, GlobalConstant.TestStartDate)
-    df = pd.read_sql(sqlQuery, GlobalConstant.DBCONN_WIND, parse_dates = {'TRADE_DT':'%Y%m%d'})
-    df = df.set_index('TRADE_DT')
-
-    return df[fieldName]
 
 
 
-def __getIndicatorFromDB_byDate(date, fieldName):
-    """ Retrieve indicators from WINDDB """
 
-    # Clean date argument to match TRADE_DT format
-    date = date.replace('-', '')
+# def __getIndicatorFromDB_byStockID(stockID, fieldName):
+#     """ Retrieve indicators from WINDDB """
     
-    # Query Database
-    sqlQuery = """
-       select TRADE_DT, s_info_windcode StockID, 1/s_val_pb_new BP, 1/s_val_pe EP, 1/s_val_pe_ttm EPttm, 1/s_val_pcf_ncf CFP,
-        1/s_val_pcf_ncfttm CFPttm, 1/s_val_pcf_ocf OCFP, 1/s_val_pcf_ocfttm OCFPttm, 1/s_val_ps SalesP,
-        1/s_val_ps_ttm SalesPttm, s_dq_turn Turnover, s_dq_freeturnover FreeTurnover, 1/s_price_div_dps DividendYield
-       from WINDDB.DBO.AShareEODDerivativeIndicator
-       where TRADE_DT='%s' """ % date
-    df = pd.read_sql(sqlQuery, GlobalConstant.DBCONN_WIND, parse_dates = {'TRADE_DT':'%Y%m%d'})
-    df = df.set_index('StockID')
+#     # Query Database
+#     sqlQuery = """
+#        select TRADE_DT, s_info_windcode StockID, 1/s_val_pb_new BP, 1/s_val_pe EP, 1/s_val_pe_ttm EPttm, 1/s_val_pcf_ncf CFP,
+#         1/s_val_pcf_ncfttm CFPttm, 1/s_val_pcf_ocf OCFP, 1/s_val_pcf_ocfttm OCFPttm, 1/s_val_ps SalesP,
+#         1/s_val_ps_ttm SalesPttm, s_dq_turn Turnover, s_dq_freeturnover FreeTurnover, 1/s_price_div_dps DividendYield
+#        from WINDDB.DBO.AShareEODDerivativeIndicator
+#        where s_info_windcode='%s'
+#        and TRADE_DT>'%s' """ % (stockID, GlobalConstant.TestStartDate)
+#     df = pd.read_sql(sqlQuery, GlobalConstant.DBCONN_WIND, parse_dates = {'TRADE_DT':'%Y%m%d'})
+#     df = df.set_index('TRADE_DT')
 
-    return df[fieldName]
-
-
-
+#     return df[fieldName]
 
 
-# print 'Working...'
+
+# def __getIndicatorFromDB_byDate(date, fieldName):
+#     """ Retrieve indicators from WINDDB """
+
+#     # Clean date argument to match TRADE_DT format
+#     date = date.replace('-', '')
+    
+#     # Query Database
+#     sqlQuery = """
+#        select TRADE_DT, s_info_windcode StockID, 1/s_val_pb_new BP, 1/s_val_pe EP, 1/s_val_pe_ttm EPttm, 1/s_val_pcf_ncf CFP,
+#         1/s_val_pcf_ncfttm CFPttm, 1/s_val_pcf_ocf OCFP, 1/s_val_pcf_ocfttm OCFPttm, 1/s_val_ps SalesP,
+#         1/s_val_ps_ttm SalesPttm, s_dq_turn Turnover, s_dq_freeturnover FreeTurnover, 1/s_price_div_dps DividendYield
+#        from WINDDB.DBO.AShareEODDerivativeIndicator
+#        where TRADE_DT='%s' """ % date
+#     df = pd.read_sql(sqlQuery, GlobalConstant.DBCONN_WIND, parse_dates = {'TRADE_DT':'%Y%m%d'})
+#     df = df.set_index('StockID')
+    
+#     return df[fieldName]
+
+
+
+
+
+print 'Working...'
 # print __getIndicatorFromDB_byStockID('600230.SH', 'BP')
 # print __getIndicatorFromDB_byDate('2015-01-03', 'BP')
-# print __getIndicatorFromDB_All('600230.SH', '2015-01-01', 'BP')
+__getIndicatorFromDB_All('600230.SH', '20150101', 'BP')
 
